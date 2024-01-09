26d2f569f84597d0f68a8fd8cd3f14d750a85b37
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2024-01-08 20:57:59 -0500

    remove calculator stuff

--- old/calc/calculator.h
+++
@@ -1,6 +0,0 @@
-#ifndef calculator_h
-#define calculator_h
-
-void calc(stringstream &ss);
-
-#endif calculator_h
--- old/calc/processInput.h
+++
@@ -1,7 +0,0 @@
-#ifndef processInput_h
-#define processInput_h
-
-void processInput(stringstream& ss)
-void hint(int i)
-
-#endif
